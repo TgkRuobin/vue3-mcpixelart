bc8aa9d7730e7ff030903ab9bb63040c9205b70b
TgkRuobin <rbin09525@gmail.com>
2026-04-06 04:16:45 +0800

    feat: 升级版本号

--- src/App.vue
+++ src/App.vue
@@ -72,8 +72,8 @@ provide('userInfo', showUser)
   <div class="toast-container position-fixed bottom-0 end-0 p-3">
     <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
       <div class="toast-header">
-        <strong class="me-auto">站长的Java生存服</strong>
-        <small>推荐</small>
+        <strong class="me-auto">站长的Java生存服招人中</strong>
+        <small>欢迎你的加入</small>
         <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
       </div>
       <div class="toast-body">
